9b6d8b91996ca74c703967fc787729bb9bb0fd1f
bhagyarsh dhumal <bhagyarsh@gmail.com>
2019-04-21 23:06:26 +0530

    decimal done

--- data.py
+++ data.py
@@ -18,69 +18,69 @@ def setData(uid, slave_id, registers, timesend, vers):
     a = registers.get(1074)[5]["value"]
     runtime_energy = int(return_int_value(a, b))
 
-    runtime_energy /= 100
+    runtime_energy /= 10
     b = registers.get(2111)[5]["value"]
     a = registers.get(2112)[5]["value"]
     month_0 = int(return_int_value(a, b))
-    month_0 /= 100
+    month_0 /= 1000
     b = registers.get(2113)[5]["value"]
     a = registers.get(2114)[5]["value"]
     month_1 = int(return_int_value(a, b))
-    month_1 /= 100
+    month_1 /= 1000
     b = registers.get(2115)[5]["value"]
     a = registers.get(2116)[5]["value"]
     month_2 = int(return_int_value(a, b))
-    month_2 /= 100
+    month_2 /= 1000
     b = registers.get(2117)[5]["value"]
     a = registers.get(2118)[5]["value"]
     month_3 = int(return_int_value(a, b))
-    month_3 /= 100
+    month_3 /= 1000
     b = registers.get(2119)[5]["value"]
     a = registers.get(2120)[5]["value"]
     month_4 = int(return_int_value(a, b))
-    month_4 /= 100
+    month_4 /= 1000
     b = registers.get(2121)[5]["value"]
     a = registers.get(2122)[5]["value"]
     month_5 = int(return_int_value(a, b))
-    month_5 /= 100
+    month_5 /= 1000
     b = registers.get(2123)[5]["value"]
     a = registers.get(2124)[5]["value"]
     month_6 = int(return_int_value(a, b))
-    month_6 /= 100
+    month_6 /= 1000
     b = registers.get(2125)[5]["value"]
     a = registers.get(2126)[5]["value"]
     month_7 = int(return_int_value(a, b))
-    month_7 /= 100
+    month_7 /= 1000
     b = registers.get(2127)[5]["value"]
     a = registers.get(2128)[5]["value"]
     month_8 = int(return_int_value(a, b))
-    month_8 /= 100
+    month_8 /= 1000
     b = registers.get(2129)[5]["value"]
     a = registers.get(2130)[5]["value"]
 
     month_9 = int(return_int_value(a, b))
-    month_9 /= 100
+    month_9 /= 1000
     b = registers.get(2131)[5]["value"]
     a = registers.get(2132)[5]["value"]
     month_10 = int(return_int_value(a, b))
-    month_10 /= 100
+    month_10 /= 1000
     b = registers.get(2133)[5]["value"]
     a = registers.get(2134)[5]["value"]
     month_11 = int(return_int_value(a, b))
-    month_11 /= 100
+    month_11 /= 1000
     b = registers.get(2135)[5]["value"]
     a = registers.get(2136)[5]["value"]
     month_12 = int(return_int_value(a, b))
-    month_12 /= 100
+    month_12 /= 1000
     data = {
         "uvid": uid,
         "slave": slave_id,
-        "pv1_voltage": int(registers.get(45056)[5]["value"])/100,
-        "pv2_voltage": int(registers.get(45059)[5]["value"])/100,
-        "pv3_voltage": int(registers.get(45062)[5]["value"])/100,
-        "pv1_current": int(registers.get(45057)[5]["value"])/100,
-        "pv2_current": int(registers.get(45060)[5]["value"])/100,
-        "pv3_current": int(registers.get(45063)[5]["value"])/100,
+        "pv1_voltage": int(registers.get(45056)[5]["value"])/10,
+        "pv2_voltage": int(registers.get(45059)[5]["value"])/10,
+        "pv3_voltage": int(registers.get(45062)[5]["value"])/10,
+        "pv1_current": int(registers.get(45057)[5]["value"]),
+        "pv2_current": int(registers.get(45060)[5]["value"]),
+        "pv3_current": int(registers.get(45063)[5]["value"]),
         "pv1_power": int(registers.get(45058)[5]["value"])/100,
         "pv2_power": int(registers.get(45061)[5]["value"])/100,
         "pv3_power": int(registers.get(45064)[5]["value"])/100,
